634f0e9075dd52b584353db0200377da99c19851
rohithkoothur <rohithedat7@gmail.com>
2021-09-09 15:14:51 +0530

    nav bar added and fetched query from search box

---
+++ Components/NavBar/NavBar.js
@@ -0,0 +1,49 @@
+import Image from 'next/image'
+import logo from '../../images/logo.png'
+import { useState } from 'react'
+const NavBar = ()=>{
+    const [pokemon, putPokemon]= useState( []);
+    
+    const [query,setQuery]=useState('');
+
+
+    return(
+        <div>
+           <nav className="navbar navbar-dark bg-dark">
+  <a className="navbar-brand"><Image src={logo} alt="logo"/>   </a>
+  <form className="form-inline">
+    <input className="form-control mr-sm-2" value={query} onChange={(e)=>{
+      
+      setQuery(e.currentTarget.value)
+    }} type="search" placeholder="Search Pokemon Here" aria-label="Search" />
+    <button className="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={(e)=>{
+        e.preventDefault();
+        console.log("searched for "+query)
+        fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
+        .then((resp,Response)=>resp.json())
+        .then(data=>{
+
+          putPokemon(data);
+          console.log(data);
+          
+
+
+        });
+
+    }
+
+
+    }>Search</button>
+  </form>
+</nav>
+
+
+
+
+
+
+        </div>
+    )
+}
+
+export default NavBar
